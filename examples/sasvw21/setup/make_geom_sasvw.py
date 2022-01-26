b9c73ff7b8c1f776d455f37c5c56b071aa6b482d
hayes2419 <hayes@utk.edu>
2022-01-25 20:01:44 -0500

    clean up ordered indices of make_geom_sasvw.py and setting up for checking and setting inDir

--- examples/sasvw21/setup/make_geom_sasvw.py
+++ examples/sasvw21/setup/make_geom_sasvw.py
@@ -3,6 +3,7 @@ sys.path.insert(0, '../../../python/')
 
 import gitr as g
 import numpy as np
+import matplotlib.pyplot as plt
 
 def make_gitr_geometry_from_solps_sasvw(gitr_geometry_filename='gitrGeometry.cfg', \
                                   solps_geomfile = 'assets/vvfile'):
@@ -28,55 +29,18 @@ def make_gitr_geometry_from_solps_sasvw(gitr_geometry_filename='gitrGeometry.cfg
     z1 = z[:-1]
     z2 = z[1:]
 
-    print(len(r), len(r1), len(r2))
+    #plot correctly-ordered line segments
+    plt.plot(r,z,linewidth=0.1)
+    plt.scatter(r, z, s=0.4)
+    plt.xlabel('r [mm]')
+    plt.ylabel('z [mm]')
+    plt.title('DIII-D SAS-VW Geometry')
+    plt.savefig('solps_geom.pdf')
+    plt.close()
 
-    import matplotlib.pyplot as plt
-    plt.plot(r,z,linewidth=0.05)
-    plt.savefig('trash.pdf')
+    #define in-direction, inDir
 
     """
-    r_west = solps_mesh[:, [0, 2]].transpose()[0, manual_indices]
-    z_west = solps_mesh[:, [1, 3]].transpose()[0, manual_indices]
-
-    plt.plot(r_west, z_west)
-    plt.xlabel('r [m]')
-    plt.ylabel('z [m]')
-    plt.title('Raw WEST Geometry from SOLPS')
-    #plt.savefig('mesh_extra_west.pdf')
-
-    #plt.scatter(r,z,s=0.4)
-    #plt.savefig('mesh_extra_west_scatter.png')
-
-
-    #get target geometries from solps
-    #print('manual geometry size',r_west.size)
-    r_left_target,z_left_target,r_right_target,z_right_target = solps.get_target_coordinates(solps_geom)
-    plt.plot(r_left_target, z_left_target)
-    plt.plot(r_right_target, z_right_target)
-    plt.title('Raw WEST Targets from SOLPS')
-    #plt.savefig('targets_west.pdf')
-
-    #integrate target geometry into base geometry
-    #uncomment print statements here and in replace_line_segments_west
-    #to help solve errors integrating targets into the base geometry
-    #print('START r_west size: ', r_west.size)
-    #print('ADD r_inner_target size: ', r_left_target.size)
-    r_final, z_final = replace_line_segment_west(r_left_target, z_left_target, r_west, z_west)
-    #print('CHECK r_final size after replacing inner target: ', r_final.size)
-    #print('ADD r_outer_target size: ', r_right_target.size)
-    r_final, z_final = replace_line_segment_west(r_right_target, z_right_target, r_final, z_final)
-    #print('CHECK r_final size after replacing outer target: ', r_final.size)
-
-    plt.close()
-    plt.plot(r_final, z_final, linewidth=0.1)
-    plt.scatter(r_final, z_final, s=0.4)
-    #plt.scatter(r_west, z_west, s=0.3)
-    plt.xlabel('r [m]')
-    plt.ylabel('z [m]')
-    plt.title('Target Geometry Integrated with WEST')
-    #plt.savefig('final_west.pdf')
-
-
     #define interior side of each line segment in the geometry with inDir
     inDir = np.ones(len(r_final))
     inDir[0:2] = inDir[3] = inDir[11:13] = inDir[14] = inDir[16] = inDir[18:21] = \
