1226979d8dc1d7ed35cb4efc0a1f7dcff5cf26ea
hayes2419 <hayes@utk.edu>
2022-01-25 19:26:37 -0500

    add matlab geometry visualization helper script

---
+++ examples/sasvw21/setup/viz_geom_sasvw.m
@@ -0,0 +1,25 @@
+close all
+d = dlmread('assets/vvfile')';
+
+indices = [1:5,7,6,8:110,112:117]
+r = d(1,indices);
+z = d(2,indices);
+r1 = r(1:end-1);
+r2 = r(2:end);
+z1 = z(1:end-1);
+z2 = z(2:end);
+
+figure(1)
+title({'DIII-D SAS-VW Cross-Sectional','Geometry'})
+xlabel('r [m]')
+ylabel('z [m]')
+set(gca,'fontsize',16)
+axis equal
+
+% figure(3)
+plot(r,z)
+hold on
+scatter(r,z)
+a = [1:length(r)]'; b = num2str(a); c = cellstr(b);
+dx = 0.01; dy = 0.01; % displacement so the text does not overlay the data points
+text(r+dx, z+dy, c);
