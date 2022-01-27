b5218865a7bd7445a97caa6d8c9aee15d99aca2b
hayes2419 <hayes@utk.edu>
2022-01-27 17:37:54 -0500

    completed make_geom_sasvw.py pending details about which boundaries are the divertor and which boundaries are W from Greg Sinclair

--- examples/sasvw21/setup/make_geom_sasvw.py
+++ examples/sasvw21/setup/make_geom_sasvw.py
@@ -24,51 +24,39 @@ def make_gitr_geometry_from_solps_sasvw(gitr_geometry_filename='gitrGeometry.cfg
 
     r = solps_geom.transpose()[0,manual_indices]
     z = solps_geom.transpose()[1,manual_indices]
-    r1 = r[:-1]
-    r2 = r[1:]
-    z1 = z[:-1]
-    z2 = z[1:]
 
     #plot correctly-ordered line segments
     plt.plot(r,z,linewidth=0.1)
-    plt.scatter(r, z, s=0.4)
+    plt.scatter(r, z, s=0.1)
+    plt.axis('scaled')
     plt.xlabel('r [mm]')
     plt.ylabel('z [mm]')
     plt.title('DIII-D SAS-VW Geometry')
-    plt.savefig('solps_geom.pdf')
-    plt.close()
+    plt.savefig('plots/solps_geom.pdf')
 
-    #define in-direction, inDir
-
-    """
     #define interior side of each line segment in the geometry with inDir
-    inDir = np.ones(len(r_final))
-    inDir[0:2] = inDir[3] = inDir[11:13] = inDir[14] = inDir[16] = inDir[18:21] = \
-            inDir[34:37] = inDir[58:61] = inDir[74] = inDir[77:] = -1
+    inDir = np.ones(len(r))
+    inDir[0:12] = inDir[15:18] = inDir[19:41] = inDir[47] = inDir[57:61] = inDir[101:103] = inDir[106:] = -1
 
     #populate lines and check that vectors point inward
-    lines = gitr_lines_from_points_west(r_final, z_final)
-    lines_to_vectors_west(lines, inDir, 'vectors_west.pdf')
+    lines = g.gitr_lines_from_points(r,z)
+    g.lines_to_vectors(lines, inDir, 'inDir', plt)
 
+    #give the divertor target segments, targ_indices, a material and an interactive surface
+    Z = np.zeros(len(r))
+    surfaces = np.zeros(len(r))
 
-    Z = np.zeros(len(r_final)+1)
-    surfaces = np.zeros(len(r_final)+1)
+    targ_indices = np.array(range(58,82))
+    Z[targ_indices] = 74;
+    surfaces[targ_indices] = 1;
 
-    i_a, i_b = intersection(r_final, z_final, r_left_target, z_left_target)
-    Z[i_b] = 74;
-    surfaces[i_b] = 1;
+    #populate geometry input file to GITR
+    g.lines_to_gitr_geometry(gitr_geometry_filename, lines, Z, surfaces, inDir)
 
-    i_a, i_b = intersection(r_final, z_final, r_right_target, z_right_target)
-    Z[i_b] = 74;
-    surfaces[i_b] = 1;
+    #g.removeQuotes(infile=gitr_geometry_filename, outfile=gitr_geometry_filename+"0")
 
-    lines_to_gitr_geometry(gitr_geometry_filename, lines, Z, surfaces, inDir)
-
-    removeQuotes(infile=gitr_geometry_filename, outfile=gitr_geometry_filename+"0")
-
-    remove_endline_after_comma(infile=gitr_geometry_filename+"0", outfile=gitr_geometry_filename+"00")
-    remove_endline_after_comma2(infile=gitr_geometry_filename+"00", outfile=gitr_geometry_filename)
-    """
+    #g.remove_endline_after_comma(infile=gitr_geometry_filename+"0", outfile=gitr_geometry_filename+"00")
+    #g.remove_endline_after_comma2(infile=gitr_geometry_filename+"00", outfile=gitr_geometry_filename)
 
 if __name__ == "__main__":
     make_gitr_geometry_from_solps_sasvw()
