d022cdf311ad947575a7a56af3adce5b31c03d33
hayes2419 <hayes@utk.edu>
2022-03-29 16:58:05 -0400

    remove all instances of triple-quotes >:)

--- examples/sasvw21/setup/setup_sasvw.py
+++ examples/sasvw21/setup/setup_sasvw.py
@@ -6,7 +6,7 @@ import shutil
 import solps
 import make_geom_sasvw
 import make_ParticleSource_sasvw
-"""
+
 r,z, rW,zW = make_geom_sasvw.V6e_v002(gitr_geometry_filename='gitrGeometry.cfg', \
                                     solps_geomfile = 'assets/geom-SASV/SAS-V6e_v002.ogr', \
                                     solps_rz = 'assets/geom-SASV/solps_rz.txt', \
@@ -20,13 +20,13 @@ solps.readEquilibrium(filename = 'assets/vertex_sasvw.eq', \
                                     r_wall = r, z_wall = z)
 
 shutil.move('bField.nc', '../input/bField.nc')
-"""
+
 solps.process_solps_output_for_gitr(dakota_filename = 'assets/dakota', \
                                    nR = 500, nZ = 1000, plot_variables=0, \
                                    b2fstate_filename = 'assets/b2fstate')
 
 shutil.move('profiles.nc', '../input/profiles.nc')
-"""
+
 solps.make_solps_targ_coord_file(gitr_geom_filename = '../input/gitrGeometry.cfg', \
                                     solps_geom = 'assets/b2fgmtry', \
                                     coords_file = 'assets/right_target_coordinates.txt', \
@@ -39,4 +39,3 @@ make_ParticleSource_sasvw.simple2D(nP = int(1e3), \
                                     r_W = rW, z_W = zW)
 
 shutil.move('particleSource.nc', '../input/particleSource.nc')
-"""
