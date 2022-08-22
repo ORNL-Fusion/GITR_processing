6d618d3b2fdee8d9a702b303a270edb64b508622
hayes2419 <hayes@utk.edu>
2022-08-22 17:19:50 -0400

    surface model problem that crashes on macbook

--- examples/sasvw21/setup/make_ParticleSource_sasvw.py
+++ examples/sasvw21/setup/make_ParticleSource_sasvw.py
@@ -283,10 +283,10 @@ def random(nP,pps_weights,adj,slope,Beta, r_coord,z_coord):
 def point_source(nP = int(1e3)):
     x = 1.49*np.ones(nP)
     y = np.zeros(nP)
-    z = 1.2*np.ones(nP)
+    z = 1.15*np.ones(nP)
     vx = 500000*np.ones(nP)
     vy = 5000*np.zeros(nP)
-    vz = 500*np.zeros(nP)
+    vz = 50*np.zeros(nP)
 
     #########################################
     #make NetCDF Particle Source file
