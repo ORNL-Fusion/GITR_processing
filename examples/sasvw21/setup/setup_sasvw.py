f5b3f2319042179467c8136067c2d4e09aad89fe
ecrouse3 <elicr@Admiral.localdomain>
2023-02-17 14:41:52 -0500

    added needed assets and changed point source arrays

--- examples/sasvw21/setup/setup_sasvw.py
+++ examples/sasvw21/setup/setup_sasvw.py
@@ -25,20 +25,23 @@ solps.readEquilibrium(filename = 'assets/vertex_sasvw.eq', \
                                     r_wall = r, z_wall = z)
 
 shutil.move('bField.nc', '../input/bField.nc')
-
+'''
+'''
 solps.process_solps_output_for_gitr(dakota_filename = 'assets/dakota', \
                                    nR = 500, nZ = 1000, plot_variables = 0, \
                                    b2fstate_filename = 'assets/b2fstate', \
                                    r_wall = r, z_wall = z)
 
 shutil.move('profiles.nc', '../input/profiles.nc')
-
+'''
+#%%
 solps.make_solps_targ_coord_file(gitr_geom_filename = '../input/gitrGeometry.cfg', \
                                     solps_geom = 'assets/b2fgmtry', \
                                     coords_file = 'assets/right_target_coordinates.txt', \
                                     right_target_filename = 'assets/rightTargOutput')
 
 make_ParticleSource_sasvw.point_source(nP = int(2e2))
+#%%
 '''
 make_ParticleSource_sasvw.simple2D(nP = int(1e4), \
                                     geom = '../input/gitrGeometry.cfg', \
@@ -53,5 +56,5 @@ make_ParticleSource_sasvw.simple2D(nP = int(1e4), \
                                     configuration = 'random', \
                                     plot_variables = 1, \
                                     r_W=rW, z_W=zW, rCoarse=rCoarse, zCoarse=zCoarse, addedPoints=addedPoints)
-
+'''
 shutil.move('particleSource.nc', '../input/particleSource.nc')
