cf171a7c40be8d0120a47f71919a367dbae456b9
hayes2419 <hayes@utk.edu>
2024-02-05 09:32:38 -0500

    added option to run setup.py for any run directory

--- examples/sasvw-pa-fav/setup/setup.py
+++ examples/sasvw-pa-fav/setup/setup.py
@@ -6,20 +6,22 @@ import numpy as np
 import solpsProcessing, makeGeom, makeParticleSource
 
 nP = int(1e5)
+run_directory = '../'
 W_indices = np.arange(11,22)
+
 print_separator = '\n-------------------------------------------------\n'
 
 print('\n',print_separator,'Making gitrGeometry.cfg',print_separator,'\n')
 makeGeom.main(gitr_geometry_filename='gitrGeometry.cfg', \
                     solps_geomfile = 'assets/sas-vw_v005_mod.ogr', \
                     solps_targfile = 'assets/b2fgmtry', \
-                    profiles_file = '../input/plasmaProfiles.nc', \
+                    profiles_file = run_directory+'input/plasmaProfiles.nc', \
                     W_indices_profiles = W_indices, \
                     numAddedPoints = 100, \
                     plot_variables = 0)
 
 os.remove('gitrGeometry.cfg0')
-shutil.move('gitrGeometry.cfg', '../input/gitrGeometry.cfg')
+shutil.move('gitrGeometry.cfg', run_directory+'input/gitrGeometry.cfg')
 
 print('\n',print_separator,'Making bField.nc',print_separator,'\n')
 solpsProcessing.readEquilibrium(equilibrium_filename = 'assets/dg.equ', \
@@ -28,7 +30,7 @@ solpsProcessing.readEquilibrium(equilibrium_filename = 'assets/dg.equ', \
                     flip_Bt = True, \
                     plot_variables = 0)
 
-shutil.move('bField.nc', '../input/bField.nc')
+shutil.move('bField.nc', run_directory+'input/bField.nc')
 '''
 solpsProcessing.plot_surf_plasma_params(W_surf = W_indices, \
                     tile_shift_indices = [1,9], \
@@ -40,11 +42,11 @@ print('\n',print_separator,'Making particleSource.nc',print_separator,'\n')
 makeParticleSource.distributed_source(nP, surfW = W_indices, \
                     tile_shift_indices = [1,9], \
                     Bangle_shift_indices = [3,8,9], \
-                    geom = '../input/gitrGeometry.cfg', \
-                    profiles_file = '../input/plasmaProfiles.nc', \
+                    geom = run_directory+'input/gitrGeometry.cfg', \
+                    profiles_file = run_directory+'input/plasmaProfiles.nc', \
                     ftDFile = 'assets/ftridynBackgroundD.nc', \
                     ftCFile = 'assets/ftridynBackgroundC.nc', \
                     configuration = 'random', \
                     plot_variables = 0)
 
-shutil.move('particleSource.nc', '../input/particleSource.nc')
+shutil.move('particleSource.nc', run_directory+'input/particleSource.nc')
