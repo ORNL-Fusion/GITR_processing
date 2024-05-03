486f096e8d340783ad3b54028a17b557cc04dc9a
hayes2419 <hayes@utk.edu>
2024-05-03 01:03:03 -0700

    add process_output bash file for using slurm to produce plots

---
+++ examples/sasvw-pa-fav/output/process_output.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A m1709
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 14:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -G 1
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=none
+
+srun python3 process_output.py
